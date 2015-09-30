95d303af4e1d86898dcc7f47da1fb6f60ba8eaf3
Kyle O'Donnell <kah.odonnell@gmail.com>
2015-09-30 20:26:09 +0000

    changing heirarchy

--- ticket/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from ticket.models import UserProfile, Event, Purchase, Ticket
-from django.contrib.auth.models import User
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(Event)
-admin.site.register(Purchase)
-admin.site.register(Ticket)
